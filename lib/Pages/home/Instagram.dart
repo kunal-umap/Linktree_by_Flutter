99ac26458f5e9e0adcc81f7c9b54ae290c2280ba
Kunal Umap <kunalgumap@gmail.com>
2023-10-01 15:36:40 +0530

    Insta Logo Change

--- lib/Pages/home/Instagram.dart
+++ lib/Pages/home/Instagram.dart
@@ -12,23 +12,37 @@ class Instagram extends StatelessWidget {
       child: SizedBox(
         width: 600,
         height: 60,
-        child: ElevatedButton.icon(
-          style: const ButtonStyle(
-            backgroundColor: MaterialStatePropertyAll(
-              Color.fromRGBO(226, 15, 210, 1),
+        child: Container(
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+              gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [
+                  Color.fromARGB(255, 249, 206, 52),
+                  Color.fromARGB(255, 238, 42, 123),
+                  Color.fromARGB(255, 98, 40, 215)
+                ],
+                // #f9ce34, #ee2a7b and #6228d7
+              )),
+          child: ElevatedButton.icon(
+            style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(
+                Color.fromRGBO(0, 0, 0, 0),
+              ),
             ),
-          ),
-          onPressed: () {},
-          icon: Image.asset(
-            "assets/images/580b57fcd9996e24bc43c521.png",
-            width: 40,
-            height: 40,
-          ),
-          label: const Text(
-            "Instagram",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
+            onPressed: () {},
+            icon: Image.asset(
+              "assets/images/580b57fcd9996e24bc43c521.png",
+              width: 40,
+              height: 40,
+            ),
+            label: const Text(
+              "Instagram",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+              ),
             ),
           ),
         ),
