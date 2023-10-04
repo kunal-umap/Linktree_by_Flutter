18313fe234b689ae0dfa2c8a00d083d4f80984c0
Yogesh <yogeshthakur3100@gmail.com>
2023-10-04 17:57:29 +0530

    UI changes

--- lib/Pages/home/home.dart
+++ lib/Pages/home/home.dart
@@ -1,8 +1,27 @@
-import 'package:flutter/material.dart';
-import 'package:linktree_manager/Pages/home/main_Pages.dart';
+import 'dart:typed_data';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:linktree_manager/Pages/home/main_Pages.dart';
+import 'package:linktree_manager/Pages/home/utilis.dart';
+
+// ignore: must_be_immutable
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Uint8List? _image;
+
+  void selectimage() async {
+    Uint8List img = await pickimage(ImageSource.gallery);
+    setState(() {
+      _image = img;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +30,7 @@ class HomePage extends StatelessWidget {
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage(
-                "assets/images/4e091d12f70ae25bad9e51226087b82f.jpg"),
+                "assets/images/f454f75292f93e2b6c66846a857edf44.jpg"),
             fit: BoxFit.cover,
           ),
         ),
@@ -21,42 +40,70 @@ class HomePage extends StatelessWidget {
               scrollDirection: Axis.vertical,
               child: Column(
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
-                    child: Image(
-                      image:
-                          AssetImage("assets/images/Google__G__Logo.svg.webp"),
-                      width: 120,
-                      height: 120,
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                    child: Stack(
+                      children: [
+                        _image != null
+                            ? GestureDetector(
+                                onTap: () {
+                                  selectimage();
+                                },
+                                child: CircleAvatar(
+                                  radius: 64,
+                                  backgroundImage: MemoryImage(_image!),
+                                ),
+                              )
+                            : GestureDetector(
+                                onTap: () {
+                                  selectimage();
+                                },
+                                child: const CircleAvatar(
+                                  radius: 64,
+                                  backgroundImage: AssetImage(
+                                      "assets/images/Default_pfp.svg.png"),
+                                ),
+                              )
+                      ],
                     ),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
-                    child: Center(
-                      child: TextField(
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Enter Your name",
-                          hintStyle: TextStyle(
-                            fontSize: 18,
-                          ),
+                  const Center(
+                    child: TextField(
+                      style: TextStyle(
+                        fontFamily: "Playfair Display",
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter Your name",
+                        hintStyle: TextStyle(
+                          fontSize: 18,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          fontWeight: FontWeight.normal,
                         ),
                       ),
                     ),
                   ),
-                  const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 15, 0, 40),
-                      child: TextField(
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Write something about yourself",
-                          hintStyle: TextStyle(
-                            fontSize: 18,
-                          ),
-                        ),
-                      )),
+                  const TextField(
+                    style: TextStyle(
+                      fontFamily: "Playfair Display",
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Write something about yourself",
+                      hintStyle: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
                   main_Pages(
                     image:
                         "assets/images/linkedin-logo-linkedin-icon-transparent-free-png.webp",
